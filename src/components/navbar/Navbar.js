209bca2170c07c1b85cba59779ea6f80d78a7179
Glrandula <lakithrandula6@gmail.com>
2025-02-07 09:56:52 +0530

    Logo updated

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -11,7 +11,11 @@ const Navbar = () => {
   return (
     <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600 pl-14 pr-14">
       <div>
-        <img src={logo} alt="logo" width={80} />
+        {/* <img src={logo} alt="logo" width={80} /> */}
+        <h2 className="text-2xl text-white font-extrabold uppercase hover:text-designColor cursor-pointer relative group overflow-hidden duration-300">
+        Lakith Randula
+        <span className='absolute left-0 bottom-0 w-full h-[2px] bg-designColor -translate-x-[100%] group-hover:translate-x-0 duration-300'/>
+        </h2>
       </div>
       <div>
         <ul className="hidden mdl:inline-flex items-center gap-6 lg:gap-10">
