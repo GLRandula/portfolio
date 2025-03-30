2aaeef9e3b66b6cfbefcb8281535aec3f6aa552e
GLRandula <lakithrandula28@gmail.com>
2025-03-31 01:57:00 +0530

    New Updates done.

--- src/components/tesimonial/Testimonial.js
+++ src/components/tesimonial/Testimonial.js
@@ -37,6 +37,8 @@ const Testimonial = () => {
        speed: 500,
        slidesToShow: 1,
        slidesToScroll: 1,
+       autoplay: true,
+       autoplaySpeed: 5000,
        nextArrow:<SampleNextArrow />,
        prevArrow:<SamplePrevArrow />,
        beforeChange: (prev, next) => {
